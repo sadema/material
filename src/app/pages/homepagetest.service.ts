f8feb75f683c49e8136b373b38a4ca7e3aadd2d8
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2019-06-07 21:26:52 +0200

    [master] minor changes

--- src/app/pages/homepagetest.service.ts
+++ src/app/pages/homepagetest.service.ts
@@ -8,7 +8,9 @@ import {Observable} from "rxjs";
 export class HomepagetestService {
 
   private homepageUrl:string = "http://localhost:6905/genericwebsite/7630e6220db0fe1eee0d66e77200937e";
-  private pageUrl:string = "http://localhost:6905/genericwebsite/7630e6220db0fe1eee0d66e7720015ae";
+  // private pageUrl:string = "http://localhost:6905/genericwebsite/7630e6220db0fe1eee0d66e7720015ae";
+  // private pageUrl:string = "http://localhost:6905/genericwebsite/851763ed7e535ad87ba41039ee000edf";
+  private pageUrl:string = "http://localhost:6905/genericwebsite/9aaf4b2279c98e95c4ae31b07f00023c";
 
   constructor(private http:HttpClient) {
   }
